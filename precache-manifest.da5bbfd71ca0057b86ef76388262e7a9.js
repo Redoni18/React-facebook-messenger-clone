4c4ac055ed0d1c711f01913d43dbb0b3f29a5562
Redoni18 <redonemini18@gmail.com>
2020-10-09 01:05:10 +0200

    Updates

---
+++ precache-manifest.da5bbfd71ca0057b86ef76388262e7a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e69bee936185562d8e9d2a071d143e",
+    "url": "/React-facebook-messenger-clone/index.html"
+  },
+  {
+    "revision": "290bf22bc82c3746da19",
+    "url": "/React-facebook-messenger-clone/static/css/main.832eb3e8.chunk.css"
+  },
+  {
+    "revision": "b6fefe7534820f8e54a1",
+    "url": "/React-facebook-messenger-clone/static/js/2.bd58cf4a.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/React-facebook-messenger-clone/static/js/2.bd58cf4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "290bf22bc82c3746da19",
+    "url": "/React-facebook-messenger-clone/static/js/main.5f726159.chunk.js"
+  },
+  {
+    "revision": "cd32ac859a82e91bf944",
+    "url": "/React-facebook-messenger-clone/static/js/runtime-main.027ccdca.js"
+  }
+]);
